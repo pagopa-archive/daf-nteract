c3edd3a85e96445507d85ea1d0f7c577fa028aca
and0111 <and.simonetta@gmail.com>
2019-02-15 13:55:19 +0100

    Daf's "select-dataset" implementation
    Keep in mind that this commit will not provide the whole functionality,
    this is only the implementation and not the full feature's integration.
    
    > The integration includes an update about the nteract's redux store
    too. By updating is meant the process of merge Daf's epics and reducers
    with nteract's ones. Which is not what we are going to aim in this
    changes, that process will comes with the "daf-nteract-packages/ducks"
    package implementation.
    
    - Adds "select-dataset" package to "daf-nteract-packages".
    
    - Adds "DatasetPopover" as "DafMenu" component in cell's toolbar found
    in "notebook-app-component/src/toolbar.tsx".
    
    - Adds custom style rules to ".bp3-menu" class in "DatasetPopover"
    component.
    
    - Implements "DatasetListSuggest" components.
    
    - Implements "datasetList" and "selectDataset" ducks.

---
+++ packages/daf-nteract-packages/select-dataset/src/highlightUtil.tsx
@@ -0,0 +1,39 @@
+const escapeRegExpChars = (value: string) =>
+  value.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+
+const highlightText = (value: string, query: string) => {
+  let lastIndex = 0;
+  const words = query
+    .split(/\s+/)
+    .filter(word => word.length > 0)
+    .map(escapeRegExpChars);
+  if (words.length === 0) {
+    return [value];
+  }
+  const regexp = new RegExp(words.join("|"), "gi");
+  const tokens: React.ReactNode[] = [];
+  while (true) {
+    const match = regexp.exec(value);
+    if (!match) {
+      break;
+    }
+    const length = match[0].length;
+    const before = value.slice(lastIndex, regexp.lastIndex - length);
+    if (before.length > 0) {
+      tokens.push(before);
+    }
+    lastIndex = regexp.lastIndex;
+    tokens.push(
+      <strong key={lastIndex} style={{ color: "hsl(210, 100%, 40%)" }}>
+        {match[0]}
+      </strong>
+    );
+  }
+  const rest = value.slice(lastIndex);
+  if (rest.length > 0) {
+    tokens.push(rest);
+  }
+  return tokens;
+};
+
+export { highlightText };
